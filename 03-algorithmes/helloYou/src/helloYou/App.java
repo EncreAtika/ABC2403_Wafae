e445937ff8db9d096c7195accfe0bf5465aa7466
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-04-25 11:28:09 +0200

    Precision consignes sur hello you

--- 03-algorithmes/helloYou/src/helloYou/App.java
+++ 03-algorithmes/helloYou/src/helloYou/App.java
@@ -1,6 +1,6 @@
 package helloYou;
 
-import java.util.Scanner; 
+import java.util.Scanner; // Besoin d'une saisi utilisateur dans ce cas il faut donc dans le cas de Java importer le scanner
 
 public class App {
 
@@ -8,12 +8,12 @@ public class App {
 		
 		String prenom; // Déclaration d'une variable "prenom" de type "chaine de caractère"
 		
-		Scanner sc; // Déclaration d'une variable de type Scanner (penser à ajouter la ligne d'import (cf ligne 3))
+		Scanner sc; // Déclaration d'une variable de type Scanner (penser à ajouter la ligne d'import (cf ligne 3)) - Le scanner est une Classe (class)
 		
-		sc = new Scanner(System.in); // Initialisation d'un nouveau scanner
+		sc = new Scanner(System.in); // Initialisation d'un nouveau scanner - comme le patron d'une couturière le scanner (class) est une sorte de modèle à recréer
 		
 		
-		System.out.println("Bonjour, veuillez saisir votre prénom: "); // Affichage
+		System.out.println("Bonjour, veuillez saisir votre prénom: "); // Affichage (c'est à dire ce qui sera lisible et demandé à l'utilisateur
 		
 		
 		prenom = sc.nextLine(); // Demande à l'utilisateur une saisie.
@@ -22,7 +22,7 @@ public class App {
 		System.out.println("Bonjour " + prenom); // Affichage
 		
 		
-		sc.close(); // Fermeture du scanner (OBLIGATOIRE)
+		sc.close(); // Fermeture du scanner (OBLIGATOIRE) car plus besoin - dès qu'on finit avec un scanner on le ferme
 		
 		
 
