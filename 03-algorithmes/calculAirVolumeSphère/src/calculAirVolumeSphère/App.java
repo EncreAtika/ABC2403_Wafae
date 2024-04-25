6b2b1a48186c046169b3b8f3a992a2ea2275198b
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-04-25 12:52:02 +0200

    Calculairevolume java première

--- 03-algorithmes/calculAirVolumeSphère/src/calculAirVolumeSphère/App.java
+++ 03-algorithmes/calculAirVolumeSphère/src/calculAirVolumeSphère/App.java
@@ -1,10 +1,41 @@
 package calculAirVolumeSphère;
 
+import java.util.Scanner;
+import java.lang.Math; // Pour les calculs dans ce cas il est necéssaire d'importer le langage
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double rayonR; // Double comme float mais 64 bits
+		double aire;
+		double volume;
+
+		
+		Scanner sc;
+		
+		sc = new Scanner(System.in); 
+		
+		System.out.println("Veuillez saisir rayonR");
+		
+		rayonR = sc.nextDouble();
+		
+		aire = (4.0 * Math.PI * Math.pow(rayonR, 2));
+		
+		volume = (4.0 * Math.PI * Math.pow(rayonR, 3) / 3);
+		
+		System.out.println("");
+		
+		System.out.println("aire = : " + aire + ".");
+		
+		System.out.println("");
+		
+		System.out.println("volume = " + volume + ".");
+		
+		sc.close();
+		
+		
+		
 	}
 
 }
