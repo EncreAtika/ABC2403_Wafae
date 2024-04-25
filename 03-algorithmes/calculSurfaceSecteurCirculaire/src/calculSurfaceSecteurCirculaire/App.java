efad5b792e2751aabd2cfee834239f3402a7aa68
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-04-25 15:04:42 +0200

    Calcul 1.5 calculSurfaceSecteurCirculaire Java

--- 03-algorithmes/calculSurfaceSecteurCirculaire/src/calculSurfaceSecteurCirculaire/App.java
+++ 03-algorithmes/calculSurfaceSecteurCirculaire/src/calculSurfaceSecteurCirculaire/App.java
@@ -1,9 +1,42 @@
 package calculSurfaceSecteurCirculaire;
 
+import java.util.Scanner;
+import java.lang.Math; // Pour les calculs dans ce cas il est necéssaire d'importer le langage java.lang.math
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		double rayonR; // "double" comme float mais 64 bits
+		double angleA;
+		double aire;
+		
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		
+		System.out.println("Veuillez saisir rayonR");
+		
+		rayonR = sc.nextDouble();
+	
+
+		
+		System.out.println("Veuillez saisir angleA");
+		
+		angleA = sc.nextDouble();
+
+		
+		aire = (Math.PI * Math.pow(rayonR, 2 * angleA) / 3);
+		
+		System.out.println("aire = : " + aire + ".");
+		
+		sc.close();
+		
+		
+		
+	
+		
 
 	}
 
