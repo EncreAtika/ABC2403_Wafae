83ecce5ab37744d729663789f817074ac9b1db47
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-04-25 15:57:13 +0200

    correction Pseudocode 1.6

--- 03-algorithmes/inversionDesValeurs/src/inversionDesValeurs/App.java
+++ 03-algorithmes/inversionDesValeurs/src/inversionDesValeurs/App.java
@@ -1,9 +1,33 @@
 package inversionDesValeurs;
 
+import java.util.Scanner;
+
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int A; // "int" nombre entier
+		int B;
+		int C;
+		
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir A");
+		A = sc.nextInt();
+		System.out.println("");
+		
+		System.out.println("Veuillez saisir B");
+		B = sc.nextInt();
+		System.out.println("");
+		
+		
+		
+		
+		
+	
 
 	}
 
