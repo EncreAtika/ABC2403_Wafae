102eb87bc8ba0e52d09b936f0798a5f0418b1934
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-04-25 08:42:34 +0200

    Référentiel dans Frité géante

--- 03-algorithmes/FriteGeante/src/friteGéante/Main.java
+++ 03-algorithmes/FriteGeante/src/friteGéante/Main.java
@@ -4,7 +4,52 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Frite Géante !"); 
-	}
 
+		System.out.println("Hello World !"); // Affiche Hello World dans la console
+		
+		
+		
+		// VARIABLES
+		
+		String prenom; // Déclaration d'une variable "prenom" de type "chaine de caractères"
+		
+		
+		
+		int age; // Déclaration d'une variable "age" de type "nombre entier 32 Bits" (valeur scalaire)
+		         // (plage de valeurs d'un INT -2 147 000 000 à +2 147 000 000)
+		
+		
+		long soldeBancaire; // Déclaration d'une variable "soldeBancaire" de type "nombre entier 64 Bits" (valeur scalaire)
+		
+		
+		
+		float tailleEnMetres; // Déclaration d'une variable "tailleEnMettres" de type "nombre réel 32 Bits" (valeur scalaire)
+		
+		
+		
+		double tailleEnMettresPourLesTresGrands; // Déclaration d'une variable "tailleEnMettresPourLesTresGrands" de type "nombre réel 64 Bits" (valeur scalaire)
+		
+		
+		
+		Boolean estMajeur; // Déclaration d'une variable "estMajeur" de type "Booléen" (valeur scalaire)
+		
+		
+		
+		prenom = "Mike"; // Affectation d'une valeur à la variable "prénom"
+		
+		
+		System.out.println("Bonjour " + prenom);
+		
+		
+		prenom = "Toto"; // Modification de la valeur de la variable "prenom"
+		
+		
+		System.out.println("Bonjour " + prenom);
+		
+		
+		
+	}
+ 
 }
+  
+
