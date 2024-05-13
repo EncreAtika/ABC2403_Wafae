622524b6f08320d9e7a38d872a259f3f0c7f1b5b
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-05-13 12:51:11 +0200

    Je  Predis l'Avenir java code error à corriger

--- 03-algorithmes/JePredisLAvenir/src/jePredisLAvenir/App.java
+++ 03-algorithmes/JePredisLAvenir/src/jePredisLAvenir/App.java
@@ -1,9 +1,52 @@
 package jePredisLAvenir;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int Heure;
+		int Minutes;
+		int MinutesPlus3;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez indiquer l'heure");
+		Heure = sc.nextInt();
+		
+		System.out.println("Veuillez indiquer les minutes");
+		Minutes = sc.nextInt();
+		
+		
+		if (Heure > 23 || Heure < 0 ) {
+			
+		}
+			
+		else if ( Minutes > 59 || Minutes < 0) {
+			System.out.println("Les données saisies sont invalides");
+		
+		} 
+		else {
+			
+		MinutesPlus3 = Minutes + 3;}
+		
+				if (MinutesPlus3 > 60) {
+					MinutesPlus3 -= 60;
+					Heure += 1;
+				}
+				
+				if (Heure > 23) {
+					Heure = 0;	
+					}
+				
+						System.out.println("Dans 3 minutes, il sera " + Heure + "h " + MinutesPlus3);
+						
+		
+				
+		
+		sc.close();
 
 	}
 
