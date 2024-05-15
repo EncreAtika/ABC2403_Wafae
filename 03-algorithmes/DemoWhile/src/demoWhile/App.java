639d0a01275a3d939afb9f12268335e8e30e8bfa
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-05-15 09:55:30 +0200

    DemoWhile  FOR ou POUR

--- 03-algorithmes/DemoWhile/src/demoWhile/App.java
+++ 03-algorithmes/DemoWhile/src/demoWhile/App.java
@@ -9,26 +9,48 @@ public class App {
 	 * 
 	 * TRAITEMENT
 	 * i <-- 0
-	 * 
-	 * Tant que i <= 10
-	 * 	ECRIRE "i vaut ", i
-	 * 	i++
-	 * FIN TANT QUE
 	 */
+	 
 	public static void main(String[] args) {
 		 
-		int i = 100;
+		int i = 0;
+		
+		/** TANT QUE i <= 10
+		 * 	ECRIRE "i vaut ", i
+		 * 	i++
+		 * FIN TANT QUE
+		 */
 		
 		while(i <= 10) {
 			System.out.println("i vaut " + i);
-			
-				i++;
+			i++;
 		}
 		
+		/** 
+		 * FAIRE 
+		 * ECRIRE "i vaut ", i
+		 * i++
+		 * TANT QUE <=10
+		 * FIN TANT QUE
+		 */
 		do {
 			System.out.println("i vaut " + i);
+			i++;
 		} while(i <= 10);
 
+	
+		int j = 0;
+		
+		/** 
+		 * 	POUR j de 0 à 10, j++) (INCREMENTER j)
+		 * 	ECRIRE "j vaut ",j
+		 * FIN POUR
+		 */
+		
+		for(j=0; j <= 10; j++ );
+				System.out.println("j vaut " + j);
+		
+		
 	}
 
 }
