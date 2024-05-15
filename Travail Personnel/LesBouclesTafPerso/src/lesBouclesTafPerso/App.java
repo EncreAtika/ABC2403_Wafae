02b9893223257a15c11e6cc713b49ab161870a76
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-05-15 11:37:20 +0200

    les boucles taf perso while

---
+++ Travail Personnel/LesBouclesTafPerso/src/lesBouclesTafPerso/App.java
@@ -0,0 +1,20 @@
+package lesBouclesTafPerso;
+
+public class App {
+
+	public static void main(String[] args) 
+	{
+	
+		int i = 0; 
+		
+		while(i != 10)
+		{
+	
+			System.out.println(i);
+			
+			i ++;
+		}
+	
+	}
+
+}
