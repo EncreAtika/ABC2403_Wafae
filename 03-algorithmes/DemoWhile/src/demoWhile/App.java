81eb96dfbdad45f7fef1fc6b01fb179f6511d821
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-05-15 09:28:23 +0200

    DemoWhile tant que <= 10

--- 03-algorithmes/DemoWhile/src/demoWhile/App.java
+++ 03-algorithmes/DemoWhile/src/demoWhile/App.java
@@ -2,11 +2,24 @@ package demoWhile;
 
 public class App {
 
+	
+	/**
+	 * VARIABLES
+	 * i est un entier
+	 * 
+	 * TRAITEMENT
+	 * i <-- 0
+	 * 
+	 * Tant que i <= 10
+	 * 	ECRIRE "i vaut ", i
+	 * 	i++
+	 * FIN TANT QUE
+	 */
 	public static void main(String[] args) {
 		 
 		int i = 0;
 		
-		while(i < 10) {
+		while(i <= 10) {
 			System.out.println("i vaut" + i);
 			
 				i++;
