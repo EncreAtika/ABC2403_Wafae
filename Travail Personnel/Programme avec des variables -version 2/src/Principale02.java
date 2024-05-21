23918ca8b36b8d205a2e2dc6a18bc8b118bbdae4
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-05-21 14:34:39 +0200

    Programme avec des variables - version 2
    Comment modifier le programme afin que l'utilisateur puisse lui même entrer le dividende et le diviseur de la division ?

--- Travail Personnel/Programme avec des variables -version 2/src/Principale02.java
+++ Travail Personnel/Programme avec des variables -version 2/src/Principale02.java
@@ -1,23 +1,34 @@
-// Ce programme calcule le quotient de 2 entiers donnés et l'affiche dans la console
+// Ce programme calcule le quotient de 2 entiers entrés par l'utilisateur et l'affiche dans la console
 
+import java.util.Scanner;
 public class Principale02 {
 
+	
 	public static void main(String[] args) {
 		
 		// Déclaration des variables
 		int diviseur;
 		double dividende, quotient;
 		
-		//Initialisation des variables 
-		dividende = 5;
-		diviseur = 2;
 		
-		//Calcul du quotient 
+		
+		// Enregistrement des valeurs tapées par l'utilisateur 
+		Scanner clavier = new Scanner(System.in);
+		
+		System.out.print("Entrer le dividende : ");
+		dividende = clavier.nextDouble();
+		System.out.print("Entrer le diviseur : ");
+		diviseur = clavier.nextInt();		
+				
+		// Calcul du quotient 
 		quotient = dividende / diviseur;
 		
 		// Affichage du résultat dans la console 
 		System.out.print("le quotient de la division est " + quotient);
+		clavier.close(); //Libération de l'espace mémoire utilisé par le Scanner
 
-	} 
+
+
+} 
 
 }
