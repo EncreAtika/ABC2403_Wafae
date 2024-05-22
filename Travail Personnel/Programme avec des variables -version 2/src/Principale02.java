1406352997af026f390e81bb77399065220ecf94
EncreAtika <166002828+EncreAtika@users.noreply.github.com>
2024-05-22 09:28:06 +0200

    Principale 02 --> Source + Format == amélioration du visuel du code

--- Travail Personnel/Programme avec des variables -version 2/src/Principale02.java
+++ Travail Personnel/Programme avec des variables -version 2/src/Principale02.java
@@ -2,33 +2,29 @@
 
 import java.util.Scanner;
 
-	public class Principale02 {
+public class Principale02 {
 
 	public static void main(String[] args) {
-		
+
 		// Déclaration des variables
 		int diviseur;
 		double dividende, quotient;
-		
-		
-		
-		// Enregistrement des valeurs tapées par l'utilisateur  
+
+		// Enregistrement des valeurs tapées par l'utilisateur
 		Scanner clavier = new Scanner(System.in);
-		
+
 		System.out.print("Entrer le dividende : ");
 		dividende = clavier.nextDouble();
 		System.out.print("Entrer le diviseur : ");
-		diviseur = clavier.nextInt();		
-				
-		// Calcul du quotient 
+		diviseur = clavier.nextInt();
+
+		// Calcul du quotient
 		quotient = dividende / diviseur;
-		
-		// Affichage du résultat dans la console 
+
+		// Affichage du résultat dans la console
 		System.out.print("le quotient de la division est " + quotient);
-		clavier.close(); //Libération de l'espace mémoire utilisé par le Scanner
-
-
-
-			} 
+		clavier.close(); // Libération de l'espace mémoire utilisé par le Scanner
 
 	}
+
+}
---
+++ Travail Personnel/Programme avec version conditionnelle - Partie 1 - Version 3/src/Principale03.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Principale03 {
+
+	public static void main(String[] args) {
+
+		// Variables
+		int codeSolde;
+		double prixArticle, prixSolde;
+
+		Scanner clavier = new Scanner(System.in);
+		System.out.print("Entrer le prix de l'article non soldé : ");
+		prixArticle = clavier.nextDouble();
+		System.out.print("Entrer le code solde, 1 pour -20% ou 2 pour -50% : ");
+		codeSolde = clavier.nextInt();
+
+		// Calcul du prix soldé et affichage
+		if (codeSolde == 1) {
+			prixSolde = 0.8 * prixArticle;
+			System.out.print("Le prix de l'article soldé à -20% est " + prixSolde + " €.");
+
+		}
+
+		else {
+			prixSolde = 0.5 * prixArticle;
+			System.out.print("Le prix de l'article soldé est à -50% est " + prixSolde + " €. ");
+		}
+
+		clavier.close();
+	}
+
+}
